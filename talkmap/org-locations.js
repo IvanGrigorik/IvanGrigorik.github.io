582e717467b19b96f5004666772699051656c6b8
Ivan Grigorik <givan502@gmail.com>
2026-08-07 16:27:37 -0400

    add locations

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,22 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Oden Institute, Austin, TX, USA",
+    30.2867525,
+    -97.7365765
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "AMD iMAGiNE symposium 2026, Austin, TX, USA",
+    30.28829126773493,
+    -97.73546934127809
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "AMD iMAGiNE symposium 2025, Austin, TX, USA",
+    30.25068012501569,
+    -97.86124348640442
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "High Performance Computing Conference 2026, Chicago, Illinois",
+    41.8915058,
+    -87.6196694
   ]
 ];
